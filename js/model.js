bdf541a15b9d107dffcea998f18f05d690bdbf93
john dupuis <johndupuis@pc-132-250.customer.ayton.knightsbridge.lan>
2016-11-04 10:31:34 -0400

    Started working on the modules

--- js/model.js
+++ js/model.js
@@ -35,4 +35,92 @@
     // use require() to import module dependencies
     var validator = require('validator'); 
   }
+
+  /***********************************************************************************
+   * @constructor
+   * Representation for dissertations
+   ***********************************************************************************/
+  function Dissertation(id,title, description, proposer, proposer_role) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.proposer = proposer;
+    this.proposer_role = proposer_role;
+  }
+
+  Dissertation.prototype = {
+    getId: function() {
+      return this.id;
+    },
+    getTitle: function() {
+      return this.title;
+    },
+    getDescription: function() {
+      return this.description;
+    },
+    getProposer: function() {
+      return this.proposer;
+    },
+    getProposerRole: function() {
+      return this.proposer_role;
+    }
+  }
+
+  /***********************************************************************************
+   * @constructor
+   * Representation for user
+   ***********************************************************************************/
+  function User(userId, role, given, surname, password) {
+    this.userId = userId;
+    this.role = role;
+    this.given = given;
+    this.surname = surname;
+    this.password = password;
+  }
+
+  function Profile(job_title, email, telephone, roomnumber, research) {
+    this.job_title = job_title;
+    this.email = email;
+    this.telephone = telephone;
+    this.roomnumber = roomnumber;
+    this.research = research;
+  }
+
+  Profile.prototype = User.prototype = {
+    getId: function() {
+      return this.userId;
+    },
+    getRole: function() {
+      return this.role;
+    },
+    getGiven: function() {
+      return this.given;
+    },
+    getSurname: function() {
+      return this.surname;
+    },
+    getPassword: function() {
+      return this.password;
+    },
+    getJobTitle: function() {
+      return this.job_title;
+    },
+    getEmail: function() {
+      return this.email;
+    },
+    getTelephone: function() {
+      return this.telephone;
+    },
+    getRoomnumber: function() {
+      return this.roomnumber;
+    },
+    getResearch: function() {
+      this.research;
+    }
+  }
+
+
+
+
+
 })();
